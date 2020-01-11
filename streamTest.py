894cf2209158a2db16f83d04142078a91283bdeb
Guillaume <gmj.vasseur@gmail.com>
2020-01-11 19:23:14 +0100

    Test script 2

--- streamTest.py
+++ streamTest.py
@@ -83,7 +83,7 @@ with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
     camera.rotation = 180
     camera.start_recording(output, format='mjpeg')
     try:
-        address = ('', 8000)
+        address = ('0.0.0.0', 8080)
         server = StreamingServer(address, StreamingHandler)
         server.serve_forever()
     finally:
