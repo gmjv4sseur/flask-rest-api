bfe18494c2a69960992209f91bb9bd3f40b3c138
Guillaume <gmj.vasseur@gmail.com>
2020-01-12 18:48:01 +0000

    add mjpeg video

--- testmp4.py
+++ testmp4.py
@@ -1,10 +1,10 @@
 from picamera import PiCamera
 import time
 
-output = "/home/pi/flask-rest-api/video.mp4"
+output = "/home/pi/flask-rest-api/video.mjpeg"
 
 camera = PiCamera()
 camera.rotation = 180
-camera.start_recording(output, format='h264')
+camera.start_recording(output, format='mjpeg')
 time.sleep(5)
 camera.stop_recording()
